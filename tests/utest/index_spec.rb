4ebc2951655c37c40fbf951092a3580e014dfc30
greg-ogs <65621681+greg-ogs@users.noreply.github.com>
2025-02-11 23:17:18 -0600

    Update Selenium image and rewrite index page tests
    Updated the Selenium image in the docker-compose file to use the latest tag. Rewrote and enhanced index page tests using Capybara with Selenium to improve coverage and ensure compatibility with the remote Selenium setup.

--- tests/utest/index_spec.rb
+++ tests/utest/index_spec.rb
@@ -1,4 +1,39 @@
-describe 'Try something' do
-    it 'Show the title' do
-        end
+require 'capybara'
+require 'capybara/dsl'
+require 'selenium-webdriver'
+include Capybara::DSL
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: 'http://selenium:4444/wd/hub',
+    options: Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      opts.args = %w[
+        --no-default-browser-check
+        --disable-gpu
+        --disable-shm
+        --no-sandbox
+        --window-size=1920x1080
+      ]
     end
+  )
+end
+Capybara.default_driver = :selenium
+
+describe 'Unitary test for index page' do
+  it 'Should be able to load the index page' do
+    visit('http://flask_app:5000')
+    expect(page).to have_current_path('http://flask_app:5000')
+    expect(page).to have_content('Docker')
+    expect(page).to have_selector('h1', text: 'Docker container flask app test')
+  end
+  it 'Should be able to load the docker page' do
+    visit('http://flask_app:5000/docker')
+    expect(page).to have_current_path('http://flask_app:5000/docker')
+  end
+  it 'Should be able to load the flask page' do
+    visit('http://flask_app:5000/flask')
+    expect(page).to have_current_path('http://flask_app:5000/flask')
+  end
+end
